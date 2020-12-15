c194c2c1884329cf0b4c47ab8a7f486a5f3ef6dc
Suraj pandey <57593524+surajpande@users.noreply.github.com>
2020-12-15 22:54:45 +0530

    Update Solution.java

--- 30-interfaces/Solution.java
+++ 30-interfaces/Solution.java
@@ -1,33 +1,17 @@
-import java.util.Scanner;
-
-interface AdvancedArithmetic {
-	int divisorSum(int n);
+interface BaseI {
+	void method();
 }
-
-// Write your code here
-class Calculator implements AdvancedArithmetic {
-	@Override
-	public int divisorSum(int n) {
-		int result = 0;
-		for (int i = 1; i <= n; i++) {
-			if (n % i == 0) {
-				result += i;
-			}
-		}
-		return result;
+class BaseC
+{
+	public void method()
+	{
+		System.out.println("Inside BaseC:: method");
 	}
 }
-
-public class Solution {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
-		scan.close();
-
-		AdvancedArithmetic myCalculator = new Calculator();
-		int sum = myCalculator.divisorSum(n);
-		System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName());
-		System.out.println(sum);
+class Implc extens BaseC implement BaseI
+{
+	public static void main(String []s)
+	{
+		(new Implc()).method();
 	}
-
 }
